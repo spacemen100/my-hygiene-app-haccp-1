2f90188c69d19c0846b1b730f08cd81728b7cba9
spacemen100 <91408506+spacemen100@users.noreply.github.com>
2025-08-09 19:02:55 +0000

    ddddddf

---
+++ components/sidebar.tsx
@@ -0,0 +1,64 @@
+// components/sidebar.tsx
+import { LayoutDashboard, Thermometer, ClipboardCheck, ClipboardList, Gauge, Settings } from "lucide-react"
+import { Button } from "./ui/button"
+import Link from "next/link"
+
+export function Sidebar() {
+  return (
+    <div className="hidden border-r bg-muted/40 md:block">
+      <div className="flex h-full max-h-screen flex-col gap-2">
+        <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
+          <Link href="/" className="flex items-center gap-2 font-semibold">
+            <span>MON APPLICATION</span>
+          </Link>
+        </div>
+        <div className="flex-1">
+          <nav className="grid items-start px-2 text-sm font-medium lg:px-4">
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary bg-muted text-primary"
+            >
+              <LayoutDashboard className="h-4 w-4" />
+              Tableau de bord
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <ClipboardCheck className="h-4 w-4" />
+              Traçabilité
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <Thermometer className="h-4 w-4" />
+              Températures
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <ClipboardList className="h-4 w-4" />
+              Hygiène et audits
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <Gauge className="h-4 w-4" />
+              Statistiques
+            </Link>
+            <Link
+              href="#"
+              className="flex items-center gap-3 rounded-lg px-3 py-2 text-muted-foreground transition-all hover:text-primary"
+            >
+              <Settings className="h-4 w-4" />
+              Paramètres
+            </Link>
+          </nav>
+        </div>
+      </div>
+    </div>
+  )
+}
